860255bd503ab6c0cf107fff3859edc2f12d4e87
Sushan10000 <sushanshakya68@gmail.com>
2024-10-17 14:40:27 +0545

    changes

--- src/veiws/AddSubject.vue
+++ src/veiws/AddSubject.vue
@@ -1,3 +1,3 @@
 <template>
-    login functionaluity 
+    login  
 </template>
--- src/veiws/AdminView.vue
+++ src/veiws/AdminView.vue
@@ -14,7 +14,6 @@ const logout = () => {
     quizStore.setAdminStatus(false);
     router.push('/');
 };
-haha
 </script>
 
 <template>
